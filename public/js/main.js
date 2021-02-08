531efe26165ba44e67531a368ca5079e793de735
marcos-dev-web <marcospuglliesyprogramer@gmail.com>
2021-02-07 21:57:34 -0300

    change title and scroll

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,7 @@
-var socket = io();
+const box = document.querySelector('.box_message');
+const socket = io();
+var nmsg = 0;
+
 window.addEventListener("onload", () => {
   document.querySelector('input[type="text"]').focus();
 });
@@ -9,12 +12,12 @@ window.addEventListener("click", () => {
 let input = document.querySelector('input[type="text"]');
 
 function newMessage(msg, cls) {
-  let box = document.querySelector('.box_message');
   let li = document.createElement('li')
   li.classList.add('msg');
   li.classList.add(cls);
   li.innerText = msg
   box.appendChild(li)
+  box.scroll(0, box.scrollHeight)
 }
 
 function send() {
@@ -22,6 +25,9 @@ function send() {
     newMessage(input.value, 'me')
     socket.emit('message', input.value);
     input.value = ""
+    document.title = "Chat"
+    nmsg = 0;
+    box.scroll(0, box.scrollHeight)
   }
 }
 
@@ -37,4 +43,6 @@ input.addEventListener('keydown', (e) => {
 
 socket.on('new_message', (msg) => {
   newMessage(msg, 'he')
+  nmsg++;
+  document.title = `Chat - (${nmsg})`;
 })
